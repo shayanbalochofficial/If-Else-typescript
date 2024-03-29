1d3b16e9e47d097fe8f018d4ad4df3810cf879d8
shayanbalochofficial <144522960+shayanbalochofficial@users.noreply.github.com>
2024-03-29 15:01:44 +0500

    Add files via upload

---
+++ Q3.ts
@@ -0,0 +1,16 @@
+//? Q3 => Write a program to determine the greater of two numbers.
+
+//! Declaring Number
+let num1: number = 10 ;
+let num2: number = 9;
+
+//! If - else statement
+if (num1 > num2) {
+    console.log(`${num1} is greater than ${num2}`);
+} else
+    if (num1 < num2) {
+    console.log(`${num2} is greater than ${num1}`);
+    }
+    else {
+        console.log('Both the numbers are equal.');
+}
