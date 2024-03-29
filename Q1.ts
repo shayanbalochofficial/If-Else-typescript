dd1789dd6c3f780cc36130702fa74a7c37634439
shayanbalochofficial <144522960+shayanbalochofficial@users.noreply.github.com>
2024-03-29 14:55:33 +0500

    Add files via upload

---
+++ Q1.ts
@@ -0,0 +1,15 @@
+//? Q1 => Write a program that check the  given number is positive or negative.
+
+//! Declaring Number
+let checkNum: number = 2;
+
+//! Executing LOOP statement
+if (checkNum > 0) {
+    console.log("Positive");
+} else
+    if(checkNum < 0){
+    console.log("Negative");
+    }
+    else {
+        console.log("Zero");
+}
---
+++ Q2.ts
@@ -0,0 +1,8 @@
+//? Q2 => Write a program that checks if a number is even or odd.
+let num: number = 12;
+
+if (num % 2 === 0) {
+    console.log("The number is even");
+} else {
+    console.log("The number is odd");
+}
