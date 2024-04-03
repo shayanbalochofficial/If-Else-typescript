7df4c56ebce4870e57336ed527a506a30e818f3e
shayanbalochofficial <144522960+shayanbalochofficial@users.noreply.github.com>
2024-04-04 01:00:14 +0500

    Add files via upload

---
+++ Q4.ts
@@ -0,0 +1,20 @@
+//? Q4 => Create a program to check a student grade and percentage.
+let marks: number = 29;
+
+//! Checking The Grade Using IF-Else
+
+if (marks >= 90) {
+    console.log("Grade: A*");
+} else if (marks >= 80) {
+    console.log("Grade: A");
+} else if (marks >= 70) {
+    console.log("Grade: B");
+} else if (marks >= 60) {
+    console.log("Grade: C");
+} else if (marks >= 50) {
+    console.log("Grade: D");
+} else if (marks >= 40) {
+    console.log("Grade: E");
+} else {
+    console.log("Grade : U\nHope You perform better Next Time.");
+}
